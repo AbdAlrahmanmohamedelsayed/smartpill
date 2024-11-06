f94eee2abb33c9c9d7851329631c55c47d98a64d
abdelrahman <104737626+AbdAlrahmanmohamedelsayed@users.noreply.github.com>
2024-11-06 16:04:29 +0200

    add app color

--- lib/core/theme/app_theme_manager.dart
+++ lib/core/theme/app_theme_manager.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:smartpill/core/theme/color_pallets.dart';
 
 class AppThemeManager {
-  static Color primary = ColorPallets.primaryColor;
+  static Color primary = AppColor.primaryColor;
   static ThemeData lightThemeManager = ThemeData(
     scaffoldBackgroundColor: Colors.white,
     primaryColor: primary,
@@ -26,17 +26,17 @@ class AppThemeManager {
           fontWeight: FontWeight.bold,
           fontFamily: 'Exo'),
       bodyLarge: TextStyle(
-          color: Colors.black,
+          color: AppColor.textColorPrimary,
           fontSize: 28,
           fontWeight: FontWeight.bold,
           fontFamily: 'Exo'),
       bodyMedium: TextStyle(
-          color: Colors.black,
+          color: AppColor.textColorPrimary,
           fontSize: 25,
           fontWeight: FontWeight.w600,
           fontFamily: 'Exo'),
       bodySmall: TextStyle(
-          color: Colors.black,
+          color: AppColor.textColorPrimary,
           fontSize: 17,
           fontWeight: FontWeight.w400,
           fontFamily: 'Exo'),
