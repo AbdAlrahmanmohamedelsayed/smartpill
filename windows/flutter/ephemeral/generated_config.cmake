80c13efaa99ec11bc177985f97bab5c131c4a01f
abdelrahman <104737626+AbdAlrahmanmohamedelsayed@users.noreply.github.com>
2025-05-22 04:28:21 +0300

    b

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\programming\\Flutter\\devtools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\programming\\Flutter\\project_smart_pill\\smartpill" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\programming\\Flutter\\devtools\\flutter"
+  "PROJECT_DIR=E:\\programming\\Flutter\\project_smart_pill\\smartpill"
+  "FLUTTER_ROOT=E:\\programming\\Flutter\\devtools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\programming\\Flutter\\project_smart_pill\\smartpill\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\programming\\Flutter\\project_smart_pill\\smartpill"
+  "FLUTTER_TARGET=E:\\programming\\Flutter\\project_smart_pill\\smartpill\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\programming\\Flutter\\project_smart_pill\\smartpill\\.dart_tool\\package_config.json"
+)
